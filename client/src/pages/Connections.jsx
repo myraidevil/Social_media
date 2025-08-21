278add044195de3c5bdcdf4cf42a968f7aecaa2b
myraidevil <rohitm10123@gmail.com>
2025-08-21 10:07:27 +0530

    made chages to feed page , messages page,

--- client/src/pages/Connections.jsx
+++ client/src/pages/Connections.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import {
+  Users,
+  UserPlus,
+  UserCheck,
+  UserRoundPen,
+  MessageSquare,
+} from "lucide-react";
+import { useNavigate } from "react-router-dom";
+import {
+  dummyConnectionsData as connections,
+  dummyFollowersData as followers,
+  dummyFollowingData as following,
+  dummyPendingConnectionsData as pendingConnections,
+  dummyUserData,
+} from "../assets/assets";
 
 const Connections = () => {
-  return (
-    <div>Connections</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default Connections
+  const dataArray = [
+    { label: "Followers", value: followers, icon: Users },
+    { label: "Following", value: following, icon: Users },
+    { label: "Pending", value: pendingConnections, icon: Users },
+    { label: "Connections", value: connections, icon: Users },
+  ];
+
+  return <div>Connections</div>;
+};
+
+export default Connections;
--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -1,11 +1,13 @@
-import React, { use, useEffect } from "react";
-import { dummyPostsData } from "../assets/assets";
+import React, { useState, useEffect } from "react";
+import { assets, dummyPostsData } from "../assets/assets";
 import Loading from "../components/Loading";
 import StoriesBar from "../components/StoriesBar";
+import PostCard from "../components/PostCard";
+import RecentMessages from "../components/RecentMessages";
 
 const Feed = () => {
-  const [feeds, setfeeds] = React.useState([]);
-  const [loading, setLoading] = React.useState(true);
+  const [feeds, setfeeds] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const fetchFeeds = async () => {
     setfeeds(dummyPostsData);
@@ -17,18 +19,29 @@ const Feed = () => {
   }, []);
 
   return !loading ? (
-    <div className="h-full overflow-y-scroll no-scrollbar py-10 xl:pr-5 flex  justify-center xl: gap-8">
+    <div className="h-full overflow-y-scroll no-scrollbar py-10 xl:pr-5 flex justify-center xl: gap-8">
       {/* stories and posts list */}
       <div>
         <StoriesBar />
-        <div>List of Post </div>
+        <div className="p-4 space-y-6">
+          {feeds.map((post) => (
+            <PostCard key={post._id} post={post} />
+          ))}
+        </div>
       </div>
       {/* right sidebar */}
-      <div>
-        <div>
-          <h1>Sponsered</h1>
+      <div className="max-xl:hidden sticky top-0">
+        <div className="max-w-xs bg-white rounded-md inline-flex flex-col gap-2 shadow p-4 ">
+          <h3 className="text-slate-800 font-semibold">Sponsored</h3>
+          <img src={assets.sponsored_img} className="w-75 h-50 rounded-md" />
+          <p className="text-slate-600">Email Marketing</p>
+          <p className="text-slate-400">
+            Supercharge yout marketing with a powerful, easy-to-use platform
+            built for results.
+          </p>
         </div>
-        <h1>recent messages</h1>
+        {/* <h1>recent messages</h1> */}
+        <RecentMessages />
       </div>
     </div>
   ) : (
