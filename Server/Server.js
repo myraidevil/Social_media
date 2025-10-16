9cc1605793323cde4e5912114784d33ba5b25c7d
myraidevil <rohitm10123@gmail.com>
2025-10-16 17:14:29 +0530

    vercel routing issue

--- Server/Server.js
+++ Server/Server.js
@@ -1,9 +1,9 @@
-import express from "express";
-import cors from "cors";
-import "dotenv/config";
-import connectDB from "./configs/db.js";
-import { inngest, functions } from "./inngest/index.js";
-import { serve } from "inngest/express";
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './configs/db.js';
+import { inngest, functions } from './inngest/index.js';
+import { serve } from 'inngest/express';
 
 const app = express();
 
@@ -12,7 +12,7 @@ await connectDB();
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => res.json({ message: "server is running" }));
-app.use("/api/inngest", serve({ client: inngest, functions }));
+app.get('/', (req, res) => res.json({ message: 'server is running' }));
+app.use('/api/inngest', serve({ client: inngest, functions }));
 
-export default app; // ✅ Don't call app.listen()
+export default app;
